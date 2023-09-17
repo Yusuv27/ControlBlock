20562af2e55124ff4388c85e68433aa8aa2f90c7
Yusuv <kingyusuwkillerspy@mail.ru>
2023-09-17 05:07:08 +0300

    Настроил цикл

--- control.py
+++ control.py
@@ -2,4 +2,7 @@ array=[]
 def Massiv(array):
     number_next=input("Введите число")
     array.append(number_next)
-    
+count=input("Сколько элементов в списке?")
+for i in range(0,count):
+    Massiv(array)
+
