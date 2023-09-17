2180697beb98747ae785e61c08932185efb4ccc4
Yusuv <kingyusuwkillerspy@mail.ru>
2023-09-17 05:59:30 +0300

    Исправляем ошибки

--- control.py
+++ control.py
@@ -1,8 +1,27 @@
 array=[]
 def Massiv(array):
-    number_next=input("Введите число")
+    number_next=int(input("Введите число: "))
     array.append(number_next)
-count=input("Сколько элементов в списке?")
+count=int(input("Введите сколько элементов в списке:"))
 for i in range(0,count):
     Massiv(array)
+print(array)
+if count < 3:
+    array=[]
+elif count == 3:
+    array_beta=[]
+    for i in array:
+        for y in range(0,1):
+            array_beta.append(i)
+    array=array_beta
+elif count > 3 :
+    count_2=0
+    for i in array:
+        count_2+=1
+        if count_2 > 1:
+            array_beta=[]
+            for y in range(0,3):
+                array_beta.append(i)
+    array=array_beta
+print(array)
 
